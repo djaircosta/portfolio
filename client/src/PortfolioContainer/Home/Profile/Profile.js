28eda2aeed778e084ceee8db09aff777a54e29ed
Djair Costa <riajd10@gmail.com>
2022-09-26 05:16:24 -0300

    make it better

---
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Typical from "react-typical";
+import ScrollService from "../../../utilities/ScrollService";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://web.facebook.com/?_rdc=1&_rdr">
+                <i className="fa fa-facebook-square" />
+              </a>
+              <a href="#">
+                <i className="fa fa-google-plus-square" />
+              </a>
+              <a href="https://www.instagram.com/devdjaircosta/">
+                <i className="fa fa-instagram" />
+              </a>
+              <a href="https://www.youtube.com/channel/UCc90vMwg_FRPV801y3Ihf4w">
+                <i className="fa fa-youtube-square" />
+              </a>
+              <a href="https://twitter.com/DevDjairCosta">
+                <i className="fa fa-twitter" />
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">Djair Costa</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "DjairCosta Dev 😎",
+                    1000,
+                    "Full stack Developer 💻",
+                    1000,
+                    "Mern stack Dev 📱",
+                    1000,
+                    "Cross Platform Dev 🔴",
+                    1000,
+                    "React/React Native 🌐",
+                    1000,
+                  ]}
+                />
+              </h1>
+            </span>
+            <span className="profile-role-tagline">
+              Knack of building applications with front and back end operations.
+            </span>
+          </div>
+
+          <div className="profile-options">
+            <button className="btn primary-btn"
+            onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+            > Hire Me </button>
+            <a href="DjairCosta.pdf" download="Djair Costa.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
